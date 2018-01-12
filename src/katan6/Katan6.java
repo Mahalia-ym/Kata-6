5c8a0fed7d5b9b067dc24caf672996a257cf1e15
Usuario <Usuario@Usuario-PC>
2018-01-12 20:31:25 +0000

    Kata 6

---
+++ src/katan6/Katan6.java
@@ -0,0 +1,16 @@
+package katan6;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Katan6 {
+    public static void main(String[] args) throws IOException {
+        String filename="C:\\Users\\Nes\\Desktop\\Clase\\3º\\IS2\\Kata4\\emailsfilev1.txt";
+        ArrayList<String> mailList=MailListReader.read(filename);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute(); 
+        
+    }
+}
